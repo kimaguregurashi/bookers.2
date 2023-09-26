d4d7cb1e2a1a7fe3ab1e0a3a3625104c68f56906
ItsukiOgawa <139129093+kimaguregurashi@users.noreply.github.com>
2023-09-26 04:27:16 +0000

    bookers2 finish

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,51 @@
+class UsersController < ApplicationController
+   before_action :is_matching_login_user, only: [:edit, :update]
+  
+  def create
+    @user = user.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome! You have signed up successfully."
+      redirect_to user_path(current_user)
+    else
+      render :users
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @books = Book.all
+  end
+  
+  def index
+    @user = current_user
+    @books = Book.all
+  end
+
+  def edit
+     @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       flash[:notice] = "You have updated user successfully."
+       redirect_to user_path(@user.id)
+    else
+       render :edit
+    end   
+  end
+
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:new, :show, :index, :create, :edit, :update, :destroy]
+  resources :users, only: [:create, :show, :index, :edit, :update]
+  get '/home/about' => 'homes#about'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
